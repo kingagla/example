3127631f844dd1c56cd3e26e700db1fadce06a3e
kinga <kingagla@gmail.com>
2026-01-02 16:08:02 +0100

    changes

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ def multiply(a, b):
     return a * b
 
 
-a, b = 3, 3
-print(f'multiplication: {a} * {b} = {multiply(a, b)}')
-print(f'Sum: {a} + {b} = {sum(a, b)}')
+# TODO: example with args and kwargs
+
+def main(a, b):
+    print(f'multiplication: {a} * {b} = {multiply(a, b)}')
+    print(f'Sum: {a} + {b} = {sum(a, b)}')
+
+if __name__ == '__main__':
+    try:
+        a, b = '3', 3
+        main(a, b)
+    except TypeError as err:
+        print(err)
+        a, b = 3, 3
+        main(a, b)
